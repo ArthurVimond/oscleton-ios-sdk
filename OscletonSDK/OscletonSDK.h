fae81af88cba4522e52402d83c33e2caffaa0ab7
Arthur Vimond <arthurvimond@gmail.com>
2018-10-09 14:56:42 +0200

    feat(*): add base project

---
+++ OscletonSDK/OscletonSDK.h
@@ -0,0 +1,19 @@
+//
+//  OscletonSDK.h
+//  OscletonSDK
+//
+//  Created by Arthur Vimond on 09/10/2018.
+//  Copyright © 2018 Arthur Vimond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OscletonSDK.
+FOUNDATION_EXPORT double OscletonSDKVersionNumber;
+
+//! Project version string for OscletonSDK.
+FOUNDATION_EXPORT const unsigned char OscletonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OscletonSDK/PublicHeader.h>
+
+
---
+++ OscletonSample/OscletonSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OscletonSample
+//
+//  Created by Arthur Vimond on 09/10/2018.
+//  Copyright © 2018 Arthur Vimond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
