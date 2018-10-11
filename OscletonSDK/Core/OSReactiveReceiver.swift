71a2a7808b7c2f3a12a3e0448afe4c7b999ba328
Arthur Vimond <arthurvimond@gmail.com>
2018-10-11 15:30:37 +0200

    feat(sdk): create OSReactiveReceiver class

---
+++ OscletonSDK/Core/OSReactiveReceiver.swift
@@ -0,0 +1,17 @@
+//
+//  OSReactiveReceiver.swift
+//  OscletonSDK
+//
+//  Created by Arthur Vimond on 10/10/2018.
+//  Copyright © 2018 Arthur Vimond. All rights reserved.
+//
+
+import Foundation
+
+public class OSReactiveReceiver {
+    
+    public func test() {
+        print("test()")
+    }
+    
+}
