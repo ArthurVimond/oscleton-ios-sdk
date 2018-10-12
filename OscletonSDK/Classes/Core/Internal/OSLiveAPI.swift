4e8101aad5dadf0dd37c7d37219dd5a5d2db328e
Arthur Vimond <arthurvimond@gmail.com>
2018-10-12 18:31:23 +0200

    feat(sdk): create OSLiveAPI class

---
+++ OscletonSDK/Classes/Core/Internal/OSLiveAPI.swift
@@ -0,0 +1,21 @@
+//
+//  OSLiveAPI.swift
+//  OscletonSDK
+//
+//  Created by Arthur Vimond on 12/10/2018.
+//  Copyright © 2018 Arthur Vimond. All rights reserved.
+//
+
+import Foundation
+
+struct OSLiveAPI {
+    
+    // Prefix
+    private static let live = "/live/"
+    
+    // Transport
+    
+    static let play = live + "play"
+    static let stop = live + "stop"
+    
+}
