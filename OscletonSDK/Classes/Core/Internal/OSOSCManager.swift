d95d2055cb7157c9b25d3a646ee6ccf8b4ad4d29
Arthur Vimond <arthurvimond@gmail.com>
2018-10-12 18:32:55 +0200

    feat(sdk): implement OSOSCManager methods

--- OscletonSDK/Classes/Core/Internal/OSOSCManager.swift
+++ OscletonSDK/Classes/Core/Internal/OSOSCManager.swift
@@ -6,11 +6,73 @@
 //
 
 import Foundation
+import SwiftOSC
 
-public class OSOSCManager {
+class OSOSCManager : OSCServerDelegate {
+    
+    private let receiver = OSCServer(address: "", port: 9001)
+    private let sender = OSCClient(address: "localhost", port: 9000)
+    
+    private let address = OSCAddressPattern("/")
     
     init() {
         print("OSOSCManager::init")
+        
+        // Set sender ip (temp)
+        sender.address = "192.168.4.186"
+        
     }
+    
+    func connect() {
+        receiver.start()
+    }
+    
+    func startListening() {
+        receiver.delegate = self
+    }
+    
+    func stopListening() {
+        receiver.delegate = nil
+    }
+    
+    func disconnect() {
+        receiver.stop()
+    }
+    
+    func sendMessage(address: String, args: [Any]? = nil) {
+        
+        let oscAddress = OSCAddressPattern(address)
+        let oscMessage = OSCMessage(oscAddress)
+        
+        if let args = args {
+            args.forEach { arg in
+                switch arg {
+                case let int as Int:
+                    oscMessage.add(int)
+                    
+                case let float as Float:
+                    oscMessage.add(float)
+                    
+                case let bool as Bool:
+                    oscMessage.add(bool)
+                    
+                case let string as String:
+                    oscMessage.add(string)
+                    
+                default: break
+                }
+            }
+        }
+        
+        sender.send(oscMessage)
+    }
+    
+    // OSCServerDelegate
+    
+    func didReceive(_ message: OSCMessage) {
+        print("didReceive - message.address: \(message.address)")
+    }
+    
+    
     
 }
