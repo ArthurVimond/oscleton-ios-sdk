330927de786838e8b042bba1489db088008f5f5d
Arthur Vimond <arthurvimond@gmail.com>
2018-10-17 10:49:16 +0200

    feat(LiveAPI): add setPeer and tempo constants

--- OscletonSDK/Classes/Core/Internal/LiveAPI.swift
+++ OscletonSDK/Classes/Core/Internal/LiveAPI.swift
@@ -13,7 +13,12 @@ struct LiveAPI {
     // Prefix
     private static let live = "/live/"
     
+    // Config
+    static let setPeer = live + "set_peer"
+    
     // Transport
+    
+    static let tempo = live + "tempo"
     
     static let play = live + "play"
     static let stop = live + "stop"
