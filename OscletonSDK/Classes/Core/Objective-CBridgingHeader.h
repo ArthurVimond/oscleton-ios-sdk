5191f03c50ff94eaa0e1a2923f9f86b045ee89db
Arthur Vimond <arthurvimond@gmail.com>
2018-10-17 10:47:11 +0200

    feat(sdk): create NetworkUtils class

---
+++ OscletonSDK/Classes/Core/Objective-CBridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  Objective-CBridgingHeader.h
+//  OscletonSDK
+//
+//  Created by Arthur Vimond on 16/10/2018.
+//
+
+#ifndef Objective_CBridgingHeader_h
+#define Objective_CBridgingHeader_h
+
+#include <ifaddrs.h>
+
+#endif /* Objective_CBridgingHeader_h */
