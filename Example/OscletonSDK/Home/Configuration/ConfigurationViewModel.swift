52f1cdef0f12176ab16fd9fd96e5b9de8d6ca53e
Arthur Vimond <arthurvimond@gmail.com>
2018-10-24 16:55:05 +0200

    feat(sample): create ConfigurationViewModel

---
+++ Example/OscletonSDK/Home/Configuration/ConfigurationViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ConfigurationViewModel.swift
+//  OscletonSDK_Example
+//
+//  Created by Arthur Vimond on 24/10/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import OscletonSDK
+
+class ConfigurationViewModel {
+    
+    let ipAddress: BehaviorSubject<String> = BehaviorSubject(value: "")
+    
+    private let bag = DisposeBag()
+    
+    init() {
+        observeProperties()
+    }
+    
+    private func observeProperties() {
+        
+        // IP address
+        ipAddress
+            .filter { !$0.isEmpty }
+            .subscribe(onNext: { ip in
+                let result = OscletonSDK.instance.configuration.setComputerIP(ip: ip)
+                if result == .error {
+                    print("Unable to setComputerIP()")
+                }
+            })
+            .disposed(by: bag)
+        
+    }
+    
+}
