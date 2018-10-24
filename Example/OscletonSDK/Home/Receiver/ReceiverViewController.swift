e9a935b33af6dcab90625127b0d49aefa5b448e3
Arthur Vimond <arthurvimond@gmail.com>
2018-10-24 13:07:14 +0200

    feat(sample): implement Receiver UI and bind ReceiverViewModel

--- Example/OscletonSDK/Home/Receiver/ReceiverViewController.swift
+++ Example/OscletonSDK/Home/Receiver/ReceiverViewController.swift
@@ -10,26 +10,75 @@ import UIKit
 import Foundation
 import OscletonSDK
 import RxSwift
+import RxCocoa
 
 class ReceiverViewController : UIViewController {
     
+    // UI
+    @IBOutlet weak var trackNameValueLabel: UILabel!
+    @IBOutlet weak var deviceNameValueLabel: UILabel!
+    @IBOutlet weak var parameterNameValueLabel: UILabel!
+    @IBOutlet weak var displayValueValueLabel: UILabel!
+    @IBOutlet weak var valueValueLabel: UILabel!
+    @IBOutlet weak var minValueLabel: UILabel!
+    @IBOutlet weak var maxValueLabel: UILabel!
+    
     private let bag = DisposeBag()
+    
+    private let viewModel = ReceiverViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Observe tempo
-        OscletonSDK.instance.receiver.rx.tempo
-            .subscribe(onNext: { tempo in
-                print("tempo.onNext: \(tempo)")
-            })
+        clearDesignTimeTexts()
+        bindProperties()
+    }
+    
+    private func clearDesignTimeTexts() {
+        trackNameValueLabel.text = ""
+        deviceNameValueLabel.text = ""
+        parameterNameValueLabel.text = ""
+        displayValueValueLabel.text = ""
+        valueValueLabel.text = ""
+        minValueLabel.text = ""
+        maxValueLabel.text = ""
+    }
+    
+    private func bindProperties() {
+        
+        // Track name
+        viewModel.trackName
+            .bind(to: trackNameValueLabel.rx.text)
             .disposed(by: bag)
         
-        // Observe device param
-        OscletonSDK.instance.receiver.rx.deviceParameter
-            .subscribe(onNext: { deviceParameter in
-                print("ViewController - deviceParameter.onNext - name: \(deviceParameter.paramName) - displayValue: \(deviceParameter.displayValue)")
-            })
+        // Device name
+        viewModel.deviceName
+            .bind(to: deviceNameValueLabel.rx.text)
+            .disposed(by: bag)
+        
+        // Parameter name
+        viewModel.paramName
+            .bind(to: parameterNameValueLabel.rx.text)
+            .disposed(by: bag)
+        
+        // Display value
+        viewModel.displayValue
+            .bind(to: displayValueValueLabel.rx.text)
+            .disposed(by: bag)
+        
+        // Value
+        viewModel.value
+            .bind(to: valueValueLabel.rx.text)
+            .disposed(by: bag)
+        
+        // Min
+        viewModel.min
+            .bind(to: minValueLabel.rx.text)
+            .disposed(by: bag)
+        
+        // Max
+        viewModel.max
+            .bind(to: maxValueLabel.rx.text)
             .disposed(by: bag)
         
     }
