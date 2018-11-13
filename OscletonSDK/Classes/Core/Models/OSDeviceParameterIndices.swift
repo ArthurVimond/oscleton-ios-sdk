736a7d0b44e03c85c70545c1a7a92d80848b02c8
Arthur Vimond <arthurvimond@gmail.com>
2018-11-13 18:13:36 +0100

    doc(OSDeviceParameterIndices): add documentation

--- OscletonSDK/Classes/Core/Models/OSDeviceParameterIndices.swift
+++ OscletonSDK/Classes/Core/Models/OSDeviceParameterIndices.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
+/**
+ Data class representing a Live device parameter indices info.
+ 
+ - Since: 0.1
+ */
 struct OSDeviceParameterIndices : Hashable {
     
+    /// The track position in the Live set
     var trackIndex: Int
+    
+    /// The device position in the track
     var deviceIndex: Int
+    
+    /// The parameter position on the device
     var paramIndex: Int
     
     init(trackIndex: Int,
