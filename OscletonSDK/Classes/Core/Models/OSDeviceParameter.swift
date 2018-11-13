e7bed5c5499a25712bf2af2b539885131550cb03
Arthur Vimond <arthurvimond@gmail.com>
2018-11-13 18:13:46 +0100

    doc(OSDeviceParameter): add documentation

--- OscletonSDK/Classes/Core/Models/OSDeviceParameter.swift
+++ OscletonSDK/Classes/Core/Models/OSDeviceParameter.swift
@@ -7,35 +7,50 @@
 
 import Foundation
 
+/**
+ Data class representing a Live device parameter.
+ 
+ - Since: 0.1
+ */
 public class OSDeviceParameter: NSObject {
     
+    /// The track position in the Live set
     @objc
     public var trackIndex: Int
     
+    /// The device position in the track
     @objc
     public var deviceIndex: Int
     
+    /// The parameter position on the device
     @objc
     public var paramIndex: Int
     
+    /// The track name containing the device
     @objc
     public var trackName: String
     
+    /// The device name containing the parameter
     @objc
     public var deviceName: String
     
+    /// The parameter name
     @objc
     public var paramName: String
     
+    /// The parameter value as displayed in Live
     @objc
     public var displayValue: String
     
+    /// The parameter value
     @objc
     public var value: Float
     
+    /// The parameter min value
     @objc
     public var min: Float
     
+    /// The parameter max value
     @objc
     public var max: Float
     
