ce098521274dbdfeaf98cc39e8058881ad22cf00
Arthur Vimond <arthurvimond@gmail.com>
2018-11-13 17:45:39 +0100

    doc(OSReactiveReceiver): add missing documentation

--- OscletonSDK/Classes/Core/OSReactiveReceiver.swift
+++ OscletonSDK/Classes/Core/OSReactiveReceiver.swift
@@ -14,14 +14,26 @@ import RxSwift
  
  If a reactive approach doesn't fit your needs, consider implementing [OSCallbackReceiver](../OSCallbackReceiver) instead.
  
- Since: 0.1
+ - Since: 0.1
  */
 public class OSReactiveReceiver: NSObject {
     
+    /**
+     Returns the Live set global tempo as Observable.
+     
+     - Returns: the Live set global tempo as Observable
+     - Since: 0.1
+     */
     public var tempo: Observable<Float> {
         get { return liveSetDataManager.tempo }
     }
     
+    /**
+     Returns the last changing device parameter as Observable.
+     
+     - Returns: the last changing device parameter as Observable
+     - Since: 0.1
+     */
     public var deviceParameter: Observable<OSDeviceParameter> {
         get { return liveSetDataManager.deviceParameter }
     }
